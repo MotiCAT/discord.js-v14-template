a3385eafc4f3d62e2c091727f2a21bb2c83149ad
MotiCAT <113648419+MotiCAT@users.noreply.github.com>
2023-04-28 21:38:32 +0900

    コメントアウトを追加

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const { Client, GatewayIntentBits, Events } = require('discord.js');
+const { Client, GatewayIntentBits, Events } = require('discord.js'); // discord.jsを使うことを宣言します。
 const client = new Client({
-    intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+    intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] // Botが監視できるもの(Intent)を指定します。
+}); 
+require('dotenv').config() // dotenvを使うことを宣言します。
+client.on(Events.ClientReady, async () => { // Botがログインしたときに発生するイベントです。
+    console.log(`Logged in as ${client.user.tag}`); // ○○としてログインした旨のログをコンソールに送信します。
 });
-require('dotenv').config()
-client.on(Events.ClientReady, async () => {
-    console.log(`Logged in as ${client.user.tag}`);
-});
-client.on(Events.MessageCreate, async message => {
-    if (message.author.bot) return;
-    if (message.content === '!ping') {
-        message.channel.send('Pong!')
+client.on(Events.MessageCreate, async message => { // メッセージが送信されたときに発生するイベントです。
+    if (message.author.bot) return; // メッセージがBotだったら処理を終了する。
+    if (message.content === '!ping') { // もしメッセージが !ping だったら
+        message.channel.send('Pong!') // Pong! と応答する
     }
 });
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN); //.envファイルの DISCORD_TOKEN を読み取りログインします。

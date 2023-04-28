abdb30a94aa154bb864fb668cfb7c12c6b96e303
MotiCAT <113648419+MotiCAT@users.noreply.github.com>
2023-04-28 21:40:12 +0900

    コメントアウト残しました。

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ client.on(Events.ClientReady, async () => { // Botがログインしたときに
 client.on(Events.MessageCreate, async message => { // メッセージが送信されたときに発生するイベントです。
     if (message.author.bot) return; // メッセージがBotだったら処理を終了する。
     if (message.content === '!ping') { // もしメッセージが !ping だったら
-        message.channel.send('Pong!') // Pong! と応答する
+        message.channel.send('Pong!'); // Pong! と応答する
+    }
+    if (message.content.match(/こんにちは/) { // 送信されたメッセージの中に こんにちは が含まれていたら
+        message.channel.send('こんにちは！'); // こんにちは！ と応答する
     }
 });
 client.login(process.env.DISCORD_TOKEN); //.envファイルの DISCORD_TOKEN を読み取りログインします。
